d35e6824d25ed4fc080d754d7f19f165a5980603
SwapnnilAgale <swapnnil.agale@gmail.com>
2022-09-01 18:01:44 +0530

    Added Number Patterns

---
+++ java/NumberPattern.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class NumberPattern
+{
+	public static void main(String[]args)
+		{
+			int count = 0;
+		for(int i=1;i<=5;i++)
+		
+			{
+				
+			for(int j=1;j<=i;j++)
+			{
+				count = count+1;
+			System.out.print(count);
+			}			
+			System.out.println();
+
+			}
+		/*
+1
+23
+456
+78910
+		*/
+		}
+
+}
---
+++ java/NumberPattern3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class NumberPattern3
+{
+	public static void main(String[]args)
+		{int count =0;
+		for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=3;j++)
+{
+count=count+1;
+System.out.print(count+" " );
+}
+System.out.println();
+}		
+		
+		}
+
+}
